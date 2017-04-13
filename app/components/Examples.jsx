d4a977cf00af5d526151158f505e596da02d376d
Urbino Pescada <urbinopescada@gmail.com>
2017-04-13 11:48:14 +0100

    Component sytles

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,16 @@
 var React = require("react");
+var {Link} = require("react-router");
 
 var Examples = (props => {
     return (
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to the examples page!</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few examples you can try out!!</p>
+<ol>
+  <li><Link to="/?location=Philadelphia">Philadelphia</Link></li>
+  <li><Link to="/?location=Lamego">Lamego</Link></li>
+  <li><Link to="/?location=Lisboa">Lisboa</Link></li>
+</ol>
       </div>
     );
 });
